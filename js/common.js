eb47f648182bf86f22de768f029cf23d5d96f86d
hrokit <hrk.kho@gmail.com>
2015-09-02 11:22:09 +0100

    enter common.js

---
+++ js/common.js
@@ -0,0 +1,8 @@
+// MixItUp is a jQuery plugin providing animated filtering and sorting.
+// https://mixitup.kunkalabs.com/
+$(function(){
+  $('#portfolio-img').mixItUp();
+});
+
+//////////////////////////////////////////////////////////////
+
